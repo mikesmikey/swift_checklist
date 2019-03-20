f92f5ed08ec0868d90a5bd7ff6c77465777678b3
informatics <informatics@informaticss-Mac-mini.local>
2019-03-20 11:45:34 +0700

    finishing chapter 12.

---
+++ Checklists/AddItemViewController.swift
@@ -0,0 +1,59 @@
+//
+//  AddItemViewControllerTableViewController.swift
+//  Checklists
+//
+//  Created by informatics on 20/2/2562 BE.
+//  Copyright © 2562 informatics. All rights reserved.
+//
+
+import UIKit
+
+class AddItemViewController: UITableViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.largeTitleDisplayMode = .never
+    }
+
+    @IBAction func back() {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func done() {
+        print("Content of the textfield : \(textField.text!)")
+        navigationController?.popViewController(animated: true)
+    }
+    
+    override func tableView(_ tableView: UITableView,
+                            willSelectRowAt indexPath: IndexPath)
+        -> IndexPath? {
+            return nil
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        textField.becomeFirstResponder()
+    }
+    
+    func textField(_ textField: UITextField,
+                   shouldChangeCharactersIn range: NSRange,
+                   replacementString string: String) -> Bool {
+        let oldText = textField.text!
+        let stringRange = Range(range, in:oldText)!
+        let newText = oldText.replacingCharacters(in: stringRange,
+                                                  with: string)
+        if newText.isEmpty {
+            doneBarButton.isEnabled = false
+        } else {
+            doneBarButton.isEnabled = true
+        }
+        return true
+    }
+    
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        doneBarButton.isEnabled = false
+        return true
+    }
+}
